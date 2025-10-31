2ed108d05a217fe48795e9ded80679c3d6007d05
ssr-1093 <159681014+ssr-1093@users.noreply.github.com>
2025-11-01 00:20:07 +0530

    Nov 1 file practice

--- test.py
+++ test.py
@@ -1,14 +1,3 @@
-import pandas as pd
+import numpy as np
 
-print('This is practice python file for git-hub')
-
-# Whatever we do here, will be tracked by Git
-
-# making second changes
-
-# making third change to check pull command
-
-# 4th change to check commit and push everthing
-
-# making CHANGES from SERVER GIT HUB - 2
-# Branch_RR is making changes
+# this is firsrt comment 
